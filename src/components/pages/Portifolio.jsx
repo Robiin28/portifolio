07c544c24a207476a00c2b726f549d8d50135921
Robel <155951971+Robiin28@users.noreply.github.com>
2025-10-06 12:49:23 +0300

    making it responsive

--- src/components/pages/Portifolio.jsx
+++ src/components/pages/Portifolio.jsx
@@ -94,19 +94,6 @@ const portfolioData = [
     note:
       "🎨 Full design and partial backend completed, system works but still needs refinement.",
     video: "/tour_guide.mp4"
-  },
-  {
-    id: 6,
-    cover: image6,
-    title: "ASAX_COMPANY",
-    name: "Ecommerce Fullstack",
-    category: "Fullstack",
-    description:
-      "Test project given for internship entrance, its Fullstack website for chemical industry. Robust backend, UI design still needs adjustments.",
-    link: "",
-    note:
-      "🎨 i have Developed this 1 year ago; robust backend design and it give me solid foundation for my future projects.",
-    video: "/asax.mp4"
   }
 ];
 
